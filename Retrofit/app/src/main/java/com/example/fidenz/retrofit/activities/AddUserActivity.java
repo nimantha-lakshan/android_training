cac5b32060225cb32fb5a4df4a729f8fb0577408
Nimantha Lakshan <lakshankkn@gmail.com>
2018-12-21 09:19:15 +0530

    add new activity

---
+++ Retrofit/app/src/main/java/com/example/fidenz/retrofit/activities/AddUserActivity.java
@@ -0,0 +1,15 @@
+package com.example.fidenz.retrofit.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fidenz.retrofit.R;
+
+public class AddUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user);
+    }
+}
